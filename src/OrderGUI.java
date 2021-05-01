981c917cadd07dc1cc063d04e78259afc9824d0f
mmtrog <minhtrong2kk63@gmail.com>
2021-05-01 17:38:07 +0700

    .

--- src/OrderGUI.java
+++ src/OrderGUI.java
@@ -1,18 +1,25 @@
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Optional;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.effect.Blend;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
@@ -46,13 +53,22 @@ public class OrderGUI extends Application {
         BackgroundFill bgfToolBar = new BackgroundFill(Color.DARKCYAN, CornerRadii.EMPTY, Insets.EMPTY);
         Background bgToolBar = new Background(bgfToolBar);
        
+        // Text
+        
+
         // Label
-        Label bottomLabel = new Label("Mai Minh Trọng || Email: trongdeptraivodich@gmail.com || Phone number: 0969696969 ");
+        Image labelIcon = new Image("image/icon.jpg", 35, 35, true, true);
+        ImageView labelIconView = new ImageView(labelIcon);
+ 
+        Label bottomLabel = new Label("Mai Minh Trọng || Email: trongdeptraivodich@gmail.com || Phone number: 0969696969", labelIconView);
+        bottomLabel.setStyle("-fx-font: 18 consolas;");
+        bottomLabel.setTextFill(Color.BLACK);
+        bottomLabel.setWrapText(true);
+        bottomLabel.setTranslateX(95);       
 
         // Image 
-        Image image = new Image("image/center.jpeg", 1015, 600 , true, true);
-        ImageView firstView = new ImageView(image);
-        firstView.autosize();
+        Image gifHello = new Image("image/gif.gif");
+        ImageView gifHelloView = new ImageView(gifHello);
 
         // Button setup
         
@@ -71,7 +87,9 @@ public class OrderGUI extends Application {
         btnShowBookContentTokens.setStyle("-fx-font: 14 arial ; -fx-base: #ffffff;");
     
         // Effect
-        
+        InnerShadow efInnerShadow = new InnerShadow();
+        efInnerShadow.setColor(Color.DARKCYAN);
+        efInnerShadow.setChoke(0.1f);
 
         // Toolbar
         ToolBar toolBar = new ToolBar(
@@ -94,27 +112,40 @@ public class OrderGUI extends Application {
         
         toolBar.autosize();
         toolBar.setBackground(bgToolBar);
+    
         
-        // HBox
-        Text textHBox = new Text("Empty HBox");
-        textHBox.autosize();
-
-        
-        // Layouts
+        // Root Pane
         BorderPane rootPane = new BorderPane();
         rootPane.setTop(toolBar);
-        rootPane.setCenter(firstView);
+        rootPane.setCenter(gifHelloView);
         rootPane.setBottom(bottomLabel);
-        rootPane.setPrefSize(900, 600);
+        rootPane.setPrefSize(1015, 600);
+
+        // Add item Pane
+        Label label = new Label("Choose type of item: ");
+        CheckBox boxDVD = new CheckBox("DVD");
+        CheckBox boxCD = new CheckBox("CD");
+        CheckBox boxBook = new CheckBox("Book");
+        Button btnChoose = new Button("Submit");
+        
+        BorderPane addItemPane = new BorderPane();
+        addItemPane.setTop(label);
 
         // Scene
         Scene rootScene = new Scene(rootPane, 1015, 600);
+        //Scene createNewOrderScene = new Scene(createNewOrderPane, 1015, 600);
+
+        
+        // First scene
+        primaryStage.setScene(rootScene);
+        primaryStage.setTitle("Order GUI");
+        primaryStage.show();
 
         // Event handle
         btnCreateNewOrder.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnCreateNewOrder.setEffect(new DropShadow());
+                btnCreateNewOrder.setEffect(efInnerShadow);
             }
         });
 
@@ -122,15 +153,15 @@ public class OrderGUI extends Application {
             @Override
             public void handle(MouseEvent evt) {
                 btnCreateNewOrder.setEffect(null);
+                //primaryStage.setScene(createNewOrderScene);
+                //primaryStage.show();
             }
         });
-
-
         
         btnAddItem.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnAddItem.setEffect(new DropShadow());
+                btnAddItem.setEffect(efInnerShadow);
             }
         });
 
@@ -144,7 +175,7 @@ public class OrderGUI extends Application {
         btnDeleteItem.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnDeleteItem.setEffect(new DropShadow());
+                btnDeleteItem.setEffect(efInnerShadow);
             }
         });
 
@@ -158,7 +189,7 @@ public class OrderGUI extends Application {
         btnDisplayTheItemsListOfOrder.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnDisplayTheItemsListOfOrder.setEffect(new DropShadow());
+                btnDisplayTheItemsListOfOrder.setEffect(efInnerShadow);
             }
         });
 
@@ -172,7 +203,7 @@ public class OrderGUI extends Application {
         btnAutoSortByCostOfItem.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnAutoSortByCostOfItem.setEffect(new DropShadow());
+                btnAutoSortByCostOfItem.setEffect(efInnerShadow);
             }
         });
 
@@ -186,7 +217,7 @@ public class OrderGUI extends Application {
         btnShowBookContentTokens.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnShowBookContentTokens.setEffect(new DropShadow());
+                btnShowBookContentTokens.setEffect(efInnerShadow);
             }
         });
 
@@ -200,7 +231,7 @@ public class OrderGUI extends Application {
         btnExit.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent evt) {
-                btnExit.setEffect(new DropShadow());
+                btnExit.setEffect(efInnerShadow);
             }
         });
 
@@ -208,18 +239,30 @@ public class OrderGUI extends Application {
             @Override
             public void handle(MouseEvent evt) {
                 btnExit.setEffect(null);
-                primaryStage.close();
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation");
+                alert.setHeaderText("Alert information: ");
+                alert.setContentText("Do you want to quit?");
+
+                ButtonType buttonTypeYes = new ButtonType("Yes", ButtonData.YES);
+                ButtonType buttonTypeNo = new ButtonType("No", ButtonData.NO);
+
+                alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == buttonTypeYes) {
+                    primaryStage.close();
+                }
+                else {
+                    
+                }
             }
         });
 
 
-        // First scene
-        primaryStage.setScene(rootScene);
-        primaryStage.setTitle("Order GUI");
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args);   
     }
 }
